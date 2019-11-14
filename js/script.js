cf13a8a1adcc9397822dac2ace2b7f46545d1268
olivia wyatt <wyatt.olivia26@gmail.com>
2019-11-14 11:36:40 -0800

    comments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 
+
+//Gets list of students
 const studentItems = document.getElementsByClassName('student-item');
 
+//Calculates number of pages needed
 const pages = () => {
   const pageNumbers = Math.ceil(studentItems.length/10) ;
   return pageNumbers;
@@ -8,6 +11,7 @@ const pages = () => {
 
 }
 
+//Function calculating starting and ending index to calculate number of students per page
 const showPage = (list,page) => {
   let itemsPerPage = 10;
 
@@ -25,8 +29,10 @@ const showPage = (list,page) => {
   }
 }
 
+//Calls showPage function
 showPage(studentItems, 1);
 
+//Function appending new div, list items, and links
 const appendPageLinks = (pages) => {
   const newDiv = document.createElement('div');
   const mainDiv = document.querySelector('.page');
@@ -45,11 +51,12 @@ const appendPageLinks = (pages) => {
     newLI.appendChild(newLink);
 
 
-    
+//Event handler for onclick event on pagination links
     newLink.addEventListener('click', () => {
       showPage(studentItems, i);
       const aLinks = document.querySelectorAll('a');
 
+//Adds 'active' class to clicked link
       for (let i = 0; i < aLinks.length; i++){
         if(aLinks[i].innerHTML === event.target.innerHTML){
           aLinks[i].className = 'active';
@@ -66,8 +73,11 @@ const appendPageLinks = (pages) => {
   }
 }
 
+//page load function to set first page link class to 'active'
 window.onload = function(){
   const aLink = document.querySelectorAll('a');
   aLink[0].className = 'active';
 }
+
+//Calls append page links function
 appendPageLinks(pages());
